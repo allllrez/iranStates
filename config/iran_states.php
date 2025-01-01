4a385832950c5bcd4184787a7bb1c9d3dca119e5
allllrez <anabestanireza@yahoo.com>
2025-01-02 00:12:28 +0330

    feat: implement Iran provinces and cities management

--- config/iran_states.php
+++ config/iran_states.php
@@ -1,6 +1,21 @@
 <?php
 
-
 return [
-    'default_state' => 'تهران',
+    /*
+    |--------------------------------------------------------------------------
+    | Iran States Configuration
+    |--------------------------------------------------------------------------
+    |
+    | This file contains the configuration for the Iran States package.
+    |
+    */
+
+    'table_names' => [
+        'states' => 'states',
+        'cities' => 'cities',
+    ],
+
+    'column_names' => [
+        'state_id' => 'state_id',
+    ],
 ];
--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -4,29 +4,30 @@ namespace Alrez\IranStates\Console;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Artisan;
+
 class InstallCommand extends Command
 {
-    protected $signature = 'iran-provinces:install';
-    protected $description = 'Install the Iran Provinces package';
+    protected $signature = 'iran-states:install';
+    protected $description = 'Install the Iran States package';
 
     public function handle()
     {
         $this->info('Publishing configurations...');
-        Artisan::call('vendor:publish --tag=config --force');
+        Artisan::call('vendor:publish', ['--tag' => 'iran-states']);
 
         $this->info('Publishing migrations...');
-        Artisan::call('vendor:publish --tag=migrations --force');
+        Artisan::call('vendor:publish', ['--tag' => 'migrations']);
 
         $this->info('Publishing seeders...');
-        Artisan::call('db:seed', ['--class' => 'StateSeeder']);
-        Artisan::call('db:seed', ['--class' => 'CitySeeder']);
+        Artisan::call('vendor:publish', ['--tag' => 'seeders']);
 
         $this->info('Running migrations...');
         Artisan::call('migrate');
 
         $this->info('Seeding database...');
-        Artisan::call('db:seed --class=CityStateSeeder');
+        Artisan::call('db:seed', ['--class' => 'StatesTableSeeder']);
+        Artisan::call('db:seed', ['--class' => 'CitiesTableSeeder']);
 
-        $this->info('Iran Provinces package installed successfully.');
+        $this->info('Iran States package installed successfully.');
     }
 }
